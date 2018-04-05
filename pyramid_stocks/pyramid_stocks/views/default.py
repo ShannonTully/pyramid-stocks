d4f76f4cc9b7fdf4168faf6c1a9a592213d31c3d
ShannonTully <scatman.st@gmail.com>
2018-04-05 16:35:03 -0700

    done

--- pyramid_stocks/pyramid_stocks/views/default.py
+++ pyramid_stocks/pyramid_stocks/views/default.py
@@ -1,44 +1,70 @@
 from pyramid.response import Response
 from pyramid.view import view_config
+from ..sample_data import MOCK_DATA
+from pyramid.httpexceptions import HTTPFound, HTTPNotFound
 
 from sqlalchemy.exc import DBAPIError
 
 from ..models import MyModel
 
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
-    try:
-        query = request.dbsession.query(MyModel)
-        one = query.filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'pyramid_stocks'}
+# @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
+# def my_view(request):
+#     try:
+#         query = request.dbsession.query(MyModel)
+#         one = query.filter(MyModel.name == 'one').first()
+#     except DBAPIError:
+#         return Response(db_err_msg, content_type='text/plain', status=500)
+#     return {'one': one, 'project': 'pyramid_stocks'}
 
 
 @view_config(route_name='home', renderer='../templates/index.jinja2', request_method='GET')
 def get_home_view(request):
-    return {}
+    return Response('Home')
 
 
-@view_config(route_name='auth', renderer='../templates/login.jinja2', request_method='GET')
+@view_config(route_name='auth', renderer='../templates/auth.jinja2')
 def get_auth_view(request):
-    return {}
+    if request.method == 'GET':
+        try:
+            username = request.GET['username']
+            password = request.GET['password']
+            print('User: {}, Pass: {}'.format(username, password))
+
+            return HTTPFound(location=request.route_url('portfolio'))
+
+        except KeyError:
+            return {}
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        print('User: {}, Pass: {}, Email: {}'.format(username, password, email))
+
+        return HTTPFound(location=request.route_url('portfolio'))
+
+    return HTTPNotFound()
 
 
 @view_config(route_name='stock', renderer='../templates/stock-add.jinja2', request_method='GET')
 def get_stock_view(request):
-    return {}
+    return Response('Stocks')
 
 
 @view_config(route_name='portfolio', renderer='../templates/portfolio.jinja2', request_method='GET')
 def get_portfolio_view(request):
-    return {}
+    return {'data': MOCK_DATA}
 
 
 @view_config(route_name='portfolio_symbol', renderer='../templates/stock-detail.jinja2', request_method='GET')
 def get_portfolio_symbol_view(request):
-    return {}
+    symbol = request.matchdict['symbol']
+
+    for data in MOCK_DATA:
+        if data['symbol'] == symbol:
+            return {'data': data}
+    return symbol
 
 
 db_err_msg = """\
